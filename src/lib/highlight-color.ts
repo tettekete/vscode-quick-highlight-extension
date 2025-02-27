15699d73000ecf050ee67ee49214091f4c6792f1
tettekete <6535634+tettekete@users.noreply.github.com>
2025-02-27 09:53:09 +0900

    improve: enhance highlight color generation and add text color function
    - Improved the algorithm for generating highlight colors.
    - Added a function to generate appropriate text colors.

--- src/lib/highlight-color.ts
+++ src/lib/highlight-color.ts
@@ -8,9 +8,24 @@ let hueStep = 130;
 
 export function getHighlightColor( prefix:string = '')
 {
-	hue = hue + hueStep * index;
+	const beforeHue = hue;	// for debug log
+	let offsetCoefficient = Math.floor( index / (360 / hueStep) );
+
+	hue = kDefaultHue + hueStep * index + offsetCoefficient * 31;
+	while( hue > 360) { hue -= 360; }
+
+	const info = {index , beforeHue , hueStep , hue, offsetCoefficient};
+	// process.stderr.write( JSON.stringify(  info, undefined,2 ));
+
 	index ++;
 	index %= Number.MAX_SAFE_INTEGER;
 
 	return prefix + RGBHexStringFromHSV( hue,1.0 ,0.94 );
+}
+
+export function getComplementaryColorForText()
+{
+	const textHue = hue + 180;
+
+	return '#' + RGBHexStringFromHSV( textHue , 1.0 , 0.94 );
 }
