2957152a8e5e94904e37321d19505773d0d69f25
tettekete <6535634+tettekete@users.noreply.github.com>
2025-03-01 11:46:15 +0900

    improve: refine destination selection algorithm for go to next/prev highlight
    - Improved logic to determine the next/previous highlight destination.
    - If no highlight matches the selected text, the cursor position is used
      to find the nearest highlight.
    - Enhances usability by making navigation more intuitive.

--- src/lib/highlight-navigator.ts
+++ src/lib/highlight-navigator.ts
@@ -2,13 +2,10 @@ import * as vscode from 'vscode';
 
 import { HighlightStore } from './highlight-store';
 import { HightLightBox } from './hight-light-box';
+import { dir } from 'console';
 
 export class HighlightNavigator
 {
-	static lastEditor:WeakRef<vscode.TextEditor> | undefined = undefined;
-	static lastRegexString: string | undefined = undefined;
-	static lastIndx: number | undefined = undefined;
-
 	static gotoNext(
 		{
 			editor,
@@ -70,6 +67,7 @@ export class HighlightNavigator
 
 		if(! regexString )
 		{
+			// create regexString and range from activeTextEditor.
 			const r = HighlightStore.getRegexAndWordFromEditor( editor );
 			regexString	= r.regex.toString();
 			range		= r.range;
@@ -81,17 +79,24 @@ export class HighlightNavigator
 		const ranges = hObj.getEditorsRanges( editor );
 		if( ranges === undefined ){ return; }
 
-		if( ! range && ! editor.selection.isEmpty )
+		if( ! range )
 		{
-			range = new vscode.Range( editor.selection.start , editor.selection.end );
+			if( ! editor.selection.isEmpty )
+			{
+				range = new vscode.Range( editor.selection.start , editor.selection.end );
+			}
 		}
 
 		let rangeIdx = 0;
 		let rangeNotFound = true;
-		if( range !== undefined )
+		if( range === undefined )
+		{
+			rangeIdx = findNearestRangeIndexFromPos( direction , editor.selection.active ,ranges );
+			rangeNotFound = false;
+		}
+		else
 		{
 			let i		= 0;
-			let found	= false;
 			for(const theRange of ranges )
 			{
 				if( theRange.isEqual( range ) )
@@ -107,18 +112,7 @@ export class HighlightNavigator
 
 		if( rangeNotFound )
 		{
-			if( cls.lastEditor?.deref() === editor
-			&& cls.lastRegexString === regexString )
-			{
-				if( cls.lastIndx === undefined )
-				{
-					cls.lastIndx = 0;
-				}
-				else
-				{
-					rangeIdx = cls.lastIndx + direction;
-				}
-			}
+			rangeIdx = findNearestRangeIndexFromPos( direction , editor.selection.active ,ranges );
 		}
 
 		rangeIdx %= ranges.length;
@@ -130,10 +124,6 @@ export class HighlightNavigator
 		editor.revealRange( ranges[rangeIdx] ,vscode.TextEditorRevealType.Default );
 		editor.selection = new vscode.Selection( ranges[rangeIdx].start , ranges[rangeIdx].end);
 
-		cls.lastEditor = new WeakRef( editor );
-		cls.lastRegexString = regexString;
-		cls.lastIndx = rangeIdx;
-
 		HightLightBox.show(
 			{
 				editor,
@@ -142,4 +132,50 @@ export class HighlightNavigator
 			}
 		);
 	}
+}
+
+
+function findNearestRangeIndexFromPos(
+	direction: 1 | -1,
+	pos: vscode.Position,
+	ranges: vscode.Range[]
+): number
+{
+	if( ranges[0].start.isAfter( pos ) )
+	{
+		return 0;
+	}
+	else if( ranges[ranges.length-1].start.isBefore( pos ))
+	{
+		return 0;
+	}
+
+	for(let i=0;i<ranges.length -1 ;i++ )
+	{
+		if( ranges[i].contains( pos ) )
+		{
+			return i + direction;
+		}
+		else if( ranges[i+1].contains( pos ) )
+		{
+			return i + 1 + direction;
+		}
+
+		const p1 = ranges[i].end;
+		const p2 = ranges[i+1].start;
+		if( p1.isBefore( pos ) && p2.isAfter(pos) )
+		{
+			if( direction > 0 )
+			{
+				return i + 1;
+			}
+			else
+			{
+				return i;
+			}
+			
+		}
+	}
+
+	return 0;
 }
